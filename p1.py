0570554aae6afb7ce3d02e505373573cd8a63fb2
Uday Hiremath <udaysh047@gmail.com>
2026-08-03 19:27:49 +0530

    Add program to check number sign

---
+++ p1.py
@@ -0,0 +1,10 @@
+# Program to check if a number is positive, negative, or zero
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("The number is Positive.")
+elif num < 0:
+    print("The number is Negative.")
+else:
+    print("The number is Zero.")
